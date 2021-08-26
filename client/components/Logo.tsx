9271287db4b61ac81da60bfc595ba7e82dee3027
Akhil Nair <4khiln4ir@gmail.com>
2021-08-26 11:04:02 +0530

    feat: Add Logo

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -16,38 +16,24 @@ const Logo = () => {
   const { colorMode } = useColorMode()
   const [isLogoLoaded, setLogoLoaded] = useState(false)
   const isMobile = useBreakpointValue(mobileBreakpointsMap)
+  const image = colorMode === ThemeMode.Dark ? './LogoDark.png' : './LogoLight.png'
+
   return (
     <AnimatePresence>
       <Link href="/" passHref>
-        {colorMode === ThemeMode.Dark ? (
-          <MotionImage
-            className={!isMobile ? styles.logo : ''}
-            boxSize={isMobile ? '30px' : '50px'}
-            objectFit="cover"
-            src="./logo.png"
-            alt="Akhil Logo"
-            fallbackSrc="./logo.png"
-            variants={simpleOpacity}
-            initial="initial"
-            animate={isLogoLoaded && 'animate'}
-            onLoad={() => setLogoLoaded(true)}
-            zIndex={2}
-          />
-        ) : (
-          <MotionImage
-            className={!isMobile ? styles.logo : ''}
-            boxSize={isMobile ? '30px' : '50px'}
-            objectFit="cover"
-            src="./logo_light.png"
-            fallbackSrc="./logo_light.png"
-            alt="KL Lawingco Logo"
-            variants={simpleOpacity}
-            initial="initial"
-            animate={isLogoLoaded && 'animate'}
-            onLoad={() => setLogoLoaded(true)}
-            zIndex={2}
-          />
-        )}
+        <MotionImage
+          className={!isMobile ? styles.logo : ''}
+          boxSize={isMobile ? '30px' : '50px'}
+          objectFit="cover"
+          src={image}
+          alt="Akhil Logo"
+          fallbackSrc="./LogoDark.png"
+          variants={simpleOpacity}
+          initial="initial"
+          animate={isLogoLoaded && 'animate'}
+          onLoad={() => setLogoLoaded(true)}
+          zIndex={2}
+        />
       </Link>
     </AnimatePresence>
   )
