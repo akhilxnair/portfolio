ea8f1cc9b0a66841d815603fca9933c3978451b5
Akhil Nair <4khiln4ir@gmail.com>
2021-09-01 16:11:57 +0530

    feat: Server Setup

---
+++ server/src/loaders/dependencyInjector.ts
@@ -0,0 +1,30 @@
+import { Container } from 'typedi';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+import LoggerInstance from './logger';
+import agendaFactory from './agenda';
+import config from '@/config';
+
+export default ({ mongoConnection, models }: { mongoConnection; models: { name: string; model: any }[] }) => {
+  try {
+    models.forEach(m => {
+      Container.set(m.name, m.model);
+    });
+
+    const agendaInstance = agendaFactory({ mongoConnection });
+    const mgInstance = new Mailgun(formData);
+
+
+    Container.set('agendaInstance', agendaInstance);
+    Container.set('logger', LoggerInstance);
+    Container.set('emailClient', mgInstance.client({ key: config.emails.apiKey, username: config.emails.apiUsername }));
+    Container.set('emailDomain', config.emails.domain);
+
+    LoggerInstance.info('✌️ Agenda injected into container');
+
+    return { agenda: agendaInstance };
+  } catch (e) {
+    LoggerInstance.error('🔥 Error on dependency injector loader: %o', e);
+    throw e;
+  }
+};
